2323e48514e82c27d54a8e46604bc4aeea3e3c34
JameseNunCuz <jamese.nuncuz.1@educa.jcyl.es>
2025-10-30 08:53:26 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>James Edward Nuñez Cuzcano</title>
+    <link rel="stylesheet" href="./webroot/css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="title">
+            <h1>Desarrollo web en entorno servidor</h1>
+            <h2>Tema 4: Técnicas de acceso a datos en PHP</h2>
+        </div>
+        <button class="home active" onclick="location.href = '../'"><img src="./webresources/home.png"></button>
+    </div>
+
+    <div class="center-container">
+
+    </div>
+
+    <div class="footer">
+        <button onclick="location.href = '../'">
+            <h3>James Edward Nuñez Cuzcano</h3>
+        </button>
+
+    </div>
+</body>
+
+</html>
